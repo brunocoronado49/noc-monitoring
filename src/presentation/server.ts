de730795e1e3fed53bd8dc8b63f1a3cf6875df83
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-08 13:54:18 -0600

    ✨ Use CronService in server class

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
+import { CronService } from './cron/cron-service';
+
 export class Server {
   public static start() {
     console.log('Server started...');
+
+    CronService.createJob('*/5 * * * * *', () => {});
   }
 }
