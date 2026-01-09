6a288876ebfbd8f6e1e998d835933a53bd20cae9
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-08 23:33:22 -0600

    🚧 Integrating DI (Dependency Injection) in the Server file

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,17 @@
+import { CheckService } from '../domain/usecases/checks/check-service';
 import { CronService } from './cron/cron-service';
 
 export class Server {
   public static start() {
     console.log('Server started...');
 
-    CronService.createJob('*/5 * * * * *', () => {});
+    CronService.createJob('*/5 * * * * *', () => {
+      const url = 'https://google.com';
+
+      new CheckService(
+        () => console.log(`Success in url: ${url}`),
+        (error: string) => console.log(error)
+      ).execute(url);
+    });
   }
 }
