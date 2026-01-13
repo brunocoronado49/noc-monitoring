4bc731c2c5a2547edf06f3c8c27d8cd091dbd437
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-13 13:45:15 -0600

    ✏️ Add typos in server and DI

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,20 @@
+import { LogRepository } from '../domain/repository/log.repository';
 import { CheckService } from '../domain/usecases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasource/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repository/ log.repository.impl';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository: LogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 
 export class Server {
   public static start() {
     console.log('Server started...');
 
     CronService.createJob('*/5 * * * * *', () => {
-      const url: string = 'https://google.com';
+      const url: string = 'https://locahost:3000';
 
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`Success in url: ${url}`),
         (error: string) => console.log(error)
       ).execute(url);
