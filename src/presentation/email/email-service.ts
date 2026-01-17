a262cba7763135952ff37351753b347aacd16ed6
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-17 00:40:42 -0600

    ✨ Create new use case and implemented in the server

--- src/presentation/email/email-service.ts
+++ src/presentation/email/email-service.ts
@@ -1,16 +1,22 @@
 import nodemailer from 'nodemailer';
 import { envs } from '../../config/plugins/env.plugins';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 interface SendMailOptions {
-  to: string;
+  to: string | string[];
   subject: string;
   htmlBody: string;
-  //TODO attachments:
+  attachments?: Attachment[];
 }
 
-// TODO: Attachments
+interface Attachment {
+  fileName: string;
+  path: string;
+}
 
 export class EmailService {
+  constructor() {}
+
   private transporter = nodemailer.createTransport({
     service: envs.MAILER_SERVICE,
     auth: {
@@ -20,20 +26,48 @@ export class EmailService {
   });
 
   async sendEmail(options: SendMailOptions): Promise<boolean> {
-    const { to, subject, htmlBody } = options;
+    const { to, subject, htmlBody, attachments = [] } = options;
 
     try {
       const sendInformation = await this.transporter.sendMail({
         to,
         subject,
         html: htmlBody,
+        attachments: attachments,
       });
 
-      console.log(sendInformation);
+      const log = new LogEntity({
+        level: LogSeverityLevel.low,
+        message: 'Email sent',
+        origin: 'email.service.ts',
+      });
 
       return true;
     } catch (error) {
+      const logFailed = new LogEntity({
+        level: LogSeverityLevel.high,
+        message: 'Email not sent',
+        origin: 'email.service.ts',
+      });
+
       return false;
     }
+  }
+
+  async sendEmailWithFilesystemLogs(to: string | string[]): Promise<boolean> {
+    const subject: string = 'Logs del servidor';
+    const htmlBody: string = `<h1>System Log</h1>`;
+    const attachments: Attachment[] = [
+      { fileName: 'logs-all.log', path: 'logs/logs-all.log' },
+      { fileName: 'logs-medium.log', path: 'logs/logs-medium.log' },
+      { fileName: 'logs-high.log', path: 'logs/logs-high.log' },
+    ];
+
+    return this.sendEmail({
+      to,
+      subject,
+      attachments,
+      htmlBody,
+    });
   }
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,27 @@
 import { LogRepository } from '../domain/repository/log.repository';
 import { CheckService } from '../domain/usecases/checks/check-service';
+import { SendEmailLogs } from '../domain/usecases/logs/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasource/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repository/ log.repository.impl';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email-service';
 
 const fileSystemLogRepository: LogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('Server started...');
 
-    // TODO: send email
-    const emailService = new EmailService();
-    emailService.sendEmail({
-      to: 'francorangelcoronado@gmail.com',
-      subject: 'MAILER TEST (System logs)',
-      htmlBody: `<h1>System Log</h1>`,
-    });
+    new SendEmailLogs(emailService, fileSystemLogRepository).execute([
+      'francorangelcoronado@gmail.com',
+      'franrangel265@gmail.com',
+    ]);
+
+    // emailService.sendEmailWithFilesystemLogs([
+    //   'francorangelcoronado@gmail.com',
+    //   'franrangel265@gmail.com',
+    // ]);
 
     // CronService.createJob('*/5 * * * * *', () => {
     //   const url: string = 'https://google.com';
