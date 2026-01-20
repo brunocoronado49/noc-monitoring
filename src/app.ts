a5c236485225b330208fa5e0e792c217c93b81e5
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-20 09:57:14 -0600

    ✨ Create mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import { envs } from './config/plugins/env.plugins';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 (async () => {
   main();
 })();
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
   Server.start();
 }
